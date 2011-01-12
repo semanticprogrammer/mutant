ce4ca714c82b11a0a88dade85be15637708f2598
‘vladperl’ <vladperl@hotmail.com>
2011-01-12 14:12:09 -0500

    routing functionality implemented as independent module

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -1,6 +1,24 @@
-var app = require('./../../lib/server');
-app.get('/', function(req, res){
-   res.send('Hello World!');
+var 
+router = require('./../../lib/router').router,
+server = require('./../../lib/server');
+
+function app(router) {
+   var 
+   self = function(req, res){
+      var route = match(req.method, req.params.pathname);
+      if (route && route.handler) {
+         route.handler.call(this, req, res);
+      }
+   };   
+   router.get('/', function(req, res){
+      res.send('Hello World!');
+   });
+   return self;
+}
+
+server.run({
+   hostname: 'localhost', 
+   port:3000, 
+   router: app(router())
 });
-app.run({hostname: 'localhost', port:3000});
 console.log('listening...');
